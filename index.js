de9bb81f2ceba185dcb259276919beb3c9ee1e7d
eyfjhurrgt <nsjcate.blevins@hotmail.com>
2024-05-08 07:44:52 +0700

    demo task commit

--- index.js
+++ index.js
@@ -1,13 +1,1 @@
-function jump(nums) {
-  let jumps = 0;
-  let currentEnd = 0;
-  let farthest = 0;
-  for (let i = 0; i < nums.length - 1; i++) {
-    farthest = Math.max(farthest, i + nums[i]);
-    if (i === currentEnd) {
-      jumps++;
-      currentEnd = farthest;
-    }
-  }
-  return jumps;
-}
+const shuffle = (arr) => arr.sort(() => Math.random() - 0.5);
